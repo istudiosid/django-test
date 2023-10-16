5a1dce95ac07488abdfce62f4a87506fd236e8b6
Agung Wiyono <wiyonoagung1@gmail.com>
2023-10-16 17:41:33 +0700

    Add history

--- app/layanan/models.py
+++ app/layanan/models.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django.db import models
 from safedelete.models import HARD_DELETE, SafeDeleteModel
 
@@ -9,9 +11,58 @@ LAYANAN_STATUS_CHOICES = (
     ("success", "Dicetak"),
 )
 
+LAYANAN_TYPE_CHOICES = (
+    ("pemerintahan", "Layanan Pemerintahan"),
+    ("umum", "Layanan Umum"),
+)
 
-class LayananPemerintahan(SafeDeleteModel):
+
+class LayananHistory(SafeDeleteModel):
     _safedelete_policy = HARD_DELETE
+
+    desa = models.ForeignKey("datamaster.Desa", on_delete=models.CASCADE)
+    obj_id = models.BigIntegerField()
+    obj_type = models.CharField(choices=LAYANAN_TYPE_CHOICES, max_length=16)
+    status = models.CharField(choices=LAYANAN_STATUS_CHOICES, max_length=16)
+    rejection_reason = models.TextField(null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        "auths.User",
+        on_delete=models.CASCADE,
+        related_name="layanan_history_created",
+    )
+    updated_by = models.ForeignKey(
+        "auths.User",
+        on_delete=models.CASCADE,
+        related_name="layanan_history_updated",
+    )
+
+    class Meta:
+        db_table = "layanan_history"
+
+
+class LayananHistoryDetail(SafeDeleteModel):
+    _safedelete_policy = HARD_DELETE
+
+    parent = models.ForeignKey(LayananHistory, on_delete=models.CASCADE)
+    status = models.CharField(choices=LAYANAN_STATUS_CHOICES, max_length=16)
+    rejection_reason = models.TextField(null=True)
+    actor = models.ForeignKey(
+        "auths.User",
+        on_delete=models.CASCADE,
+        related_name="history_detail_actor",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "layanan_history_detail"
+
+
+class LayananBaseModel(SafeDeleteModel):
+    _safedelete_policy = HARD_DELETE
+    tipe: str
 
     desa = models.ForeignKey("datamaster.Desa", on_delete=models.CASCADE)
     keperluan = models.CharField(max_length=32)
@@ -23,38 +74,32 @@ class LayananPemerintahan(SafeDeleteModel):
     created_by = models.ForeignKey(
         "auths.User",
         on_delete=models.CASCADE,
-        related_name="layanan_pemerintahan_created",
+        related_name="%(class)s_created",
     )
     updated_by = models.ForeignKey(
         "auths.User",
         on_delete=models.CASCADE,
-        related_name="layanan_pemerintahan_updated",
+        related_name="%(class)s_updated",
     )
+
+    def get_history(self) -> Optional[LayananHistory]:
+        return LayananHistory.objects.filter(
+            obj_id=self.pk, obj_type=self.tipe
+        ).first()
+
+    class Meta:
+        abstract = True
+
+
+class LayananPemerintahan(LayananBaseModel):
+    tipe = "pemerintahan"
 
     class Meta:
         db_table = "layanan_pemerintahan"
 
 
-class LayananUmum(SafeDeleteModel):
-    _safedelete_policy = HARD_DELETE
-
-    desa = models.ForeignKey("datamaster.Desa", on_delete=models.CASCADE)
-    keperluan = models.CharField(max_length=32)
-    keterangan = models.CharField(max_length=32)
-    status = models.CharField(choices=LAYANAN_STATUS_CHOICES, max_length=16)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(
-        "auths.User",
-        on_delete=models.CASCADE,
-        related_name="layanan_umum_created",
-    )
-    updated_by = models.ForeignKey(
-        "auths.User",
-        on_delete=models.CASCADE,
-        related_name="layanan_umum_updated",
-    )
+class LayananUmum(LayananBaseModel):
+    tipe = "umum"
 
     class Meta:
         db_table = "layanan_umum"
